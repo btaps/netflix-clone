54eb26877d6e96fdc58a7ebdbf9a49e4e64d9d0d
Bruno Tapia Sierra <btapiasierra@outlook.com>
2021-03-22 20:30:25 -0700

    Removed react-youtube & movie-trailer node packages because of slowing performance

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios'
-import YouTube from "react-youtube"
-import movieTrailer from "movie-trailer"
 import './Row.css'
 
 const baseURL = "https://image.tmdb.org/t/p/original"
@@ -20,29 +18,11 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
     fetchData();
   }, [fetchUrl])
 
-    const opts = {
-      height: '390',
-      width: '100%',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
-      }
-    }
-
-
-
-  const handleClick = (movie) => {
-    
-    if(trailerUrl){
+  const handleClick = (movieId) => {
+    if (trailerUrl) {
       setTrailerUrl('')
-    }else{
-      movieTrailer(movie?.name ||  movie?.title || "")
-        .then(url => {
-          const urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParams.get('v'))
-          console.log('hit')
-        })
-        .catch(error => console.log(error))
+    } else {
+      setTrailerUrl(movieId)
     }
   };
 
@@ -52,20 +32,31 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
         <h2>{title}</h2>
         <div className="row__posters">
           {movies.map((movie) => {
+            let youtubeId = "";
+
+            async function fetchYoutubeId() {
+              const response = await fetch(`http://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=39ca45225d9e96cbe5a1dbfe95818617`)
+                .catch(error => console.log(`Fetch error: ${error}`))
+              const data = await response.json()
+              youtubeId = data.results ? (data.results.length > 0 ? data.results[0].key : "") : ""
+            }
+
+            fetchYoutubeId()
+
             return movie.poster_path ? (
-              <img 
-                key={movie.id} 
-                onClick={() => handleClick(movie)}
-                className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
-                src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                alt={movie.name} 
+              <img
+                key={youtubeId ? youtubeId : movie.id}
+                onClick={() => handleClick(youtubeId ? youtubeId : movie.id)}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                alt={movie.name}
               />) : null
           })}
-                </div>
-                  {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+        </div>
+        {trailerUrl && <iframe src={`http://www.youtube.com/embed/${trailerUrl}?autoplay=1`} width="100%" height="395" frameborder="0" allowfullscreen title={trailerUrl} />}
       </div>
     </React.Fragment>
   )
 };
 
-export default Row; 
+export default Row;
